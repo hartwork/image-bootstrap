b09db259dbb750c67b852afebba85843fcf4b394
Sebastian Pipping <sebastian@pipping.org>
2015-03-07 00:19:20 +0100

    Add debugging helper script

---
+++ scripts/debug.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under AGPL v3 or later
+
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo "Called as:"
+echo "  \$0 = \"$0"\"
+echo "  \$@ = <$@>"
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo Environment:
+env
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
