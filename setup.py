8d5d25c3ba3b4a5f398a53a02f82236137347ca4
Sebastian Pipping <sebastian@pipping.org>
2015-03-16 17:19:17 +0100

    Make distutils include distros package

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from distutils.core import setup
 from image_bootstrap.metadata import GITHUB_HOME_URL, PACKAGE_NAME, VERSION_STR
 
 
+_PYTHON_PACKAGE_NAME = PACKAGE_NAME.replace('-','_')
+
+
 if __name__ == '__main__':
     setup(
             name=PACKAGE_NAME,
@@ -15,6 +18,9 @@ if __name__ == '__main__':
             author='Sebastian Pipping',
             author_email='sebastian@pipping.org',
             url=GITHUB_HOME_URL,
-            packages=[PACKAGE_NAME.replace('-','_')],
+            packages=[
+                _PYTHON_PACKAGE_NAME,
+                '%s.distros' % _PYTHON_PACKAGE_NAME,
+            ],
             data_files=[('sbin/', [PACKAGE_NAME])],
             )
