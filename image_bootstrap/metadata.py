1ed013e8dcd52fcbc130af57362e9453c0e57bd5
Sebastian Pipping <sebastian@pipping.org>
2015-03-23 23:28:39 +0100

    Bump release date and version

--- image_bootstrap/metadata.py
+++ image_bootstrap/metadata.py
@@ -7,8 +7,8 @@ GITHUB_HOME_URL = 'https://github.com/hartwork/image-bootstrap'
 
 DESCRIPTION = 'Command line tool for creating bootable virtual machine images'
 
-_VERSION = (0, 6, 5)
+_VERSION = (0, 6, 6)
 VERSION_STR = '.'.join((str(e) for e in _VERSION))
 
-_RELEASE_DATE = (2015, 3, 22)
+_RELEASE_DATE = (2015, 3, 23)
 RELEASE_DATE_STR = '-'.join(('%02d' % e for e in _RELEASE_DATE))
