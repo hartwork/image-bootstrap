0c28827e8ab292ac5746518266997cb73c3bc933
Sebastian Pipping <sebastian@pipping.org>
2016-12-28 16:54:44 +0100

    directory-bootstrap: Add basic support for Fedora (issue #14)

---
+++ directory_bootstrap/distros/fedora.py
@@ -0,0 +1,107 @@
+# Copyright (C) 2016 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under AGPL v3 or later
+
+from __future__ import print_function
+
+import os
+import shutil
+import tempfile
+
+from textwrap import dedent
+
+from directory_bootstrap.distros.base import DirectoryBootstrapper
+from directory_bootstrap.shared.commands import COMMAND_YUM
+
+
+class FedoraBootstrapper(DirectoryBootstrapper):
+    DISTRO_KEY = 'fedora'
+    DISTRO_NAME_LONG = 'Fedora'
+
+    def __init__(self, messenger, executor, abs_target_dir, abs_cache_dir, releasever):
+        super(FedoraBootstrapper, self).__init__(
+                messenger,
+                executor,
+                abs_target_dir,
+                abs_cache_dir,
+                )
+        self._releasever = releasever
+
+    def wants_to_be_unshared(self):
+        return True
+
+    @staticmethod
+    def get_commands_to_check_for():
+        return DirectoryBootstrapper.get_commands_to_check_for() + [
+                COMMAND_YUM,
+                ]
+
+    @classmethod
+    def add_arguments_to(clazz, distro):
+        distro.add_argument('--release', metavar='VERSION', default=24,
+                help='release to bootstrap (default: %(default)s)')
+
+    @classmethod
+    def create(clazz, messenger, executor, options):
+        return clazz(
+                messenger,
+                executor,
+                os.path.abspath(options.target_dir),
+                os.path.abspath(options.cache_dir),
+                options.release,
+                )
+
+    def _bootstrap_using_yum(self, abs_yum_home_dir, abs_yum_conf_path):
+        env = os.environ.copy()
+        env['HOME'] = abs_yum_home_dir
+
+        argv = [
+            COMMAND_YUM,
+            '--assumeyes',
+            '--config', abs_yum_conf_path,
+            '--installroot', self._abs_target_dir,
+            '--releasever', str(self._releasever),
+            'install', '@core',
+        ]
+        self._executor.check_call(argv, env)
+
+    def _ensure_proper_dbpath(self, abs_yum_home_dir):
+        """
+        Debian patches rpm's dbpath default to something other than
+        /var/lib/rpm, so we bypass that change to have "rpm -qa" work
+        as expected in the resulting chroot.
+        """
+        with open(os.path.join(abs_yum_home_dir, '.rpmmacros'), 'w') as f:
+            print('%_dbpath /var/lib/rpm', file=f)
+
+    def _write_yum_conf(self, abs_yum_conf_path):
+        with open(abs_yum_conf_path, 'w') as f:
+            # TODO Use key of release X, current key is release 24
+            print(dedent("""\
+                    [fedora]
+                    name=Fedora $releasever - $basearch
+                    failovermethod=priority
+                    metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-$releasever&arch=$basearch
+                    enabled=1
+                    metadata_expire=7d
+                    gpgcheck=1
+                    gpgkey=https://getfedora.org/static/81B46521.txt
+                    skip_if_unavailable=False
+                    """), file=f)
+
+    def run(self):
+        self.ensure_directories_writable()
+
+        abs_temp_dir = os.path.abspath(tempfile.mkdtemp())
+        try:
+            abs_yum_home_dir = os.path.join(abs_temp_dir, 'home')
+            os.mkdir(abs_yum_home_dir)
+
+            self._ensure_proper_dbpath(abs_yum_home_dir)
+
+            abs_yum_conf_path = os.path.join(abs_temp_dir, 'yum.conf')
+            self._write_yum_conf(abs_yum_conf_path)
+
+            self._bootstrap_using_yum(abs_yum_home_dir, abs_yum_conf_path)
+        finally:
+            self._messenger.info('Cleaning up "%s"...' % abs_temp_dir)
+            shutil.rmtree(abs_temp_dir)
