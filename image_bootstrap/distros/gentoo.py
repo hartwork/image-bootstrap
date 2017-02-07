c1729cac49ec8b808f72ef3dbe63227098c7cd4c
Sebastian Pipping <sebastian@pipping.org>
2017-02-08 00:31:48 +0100

    Gentoo: Exclude cloud-init 0.7.6_p1212, as well

--- image_bootstrap/distros/gentoo.py
+++ image_bootstrap/distros/gentoo.py
@@ -257,7 +257,7 @@ class GentooStrategy(DistroStrategy):
         return '/boot/vmlinuz'
 
     def install_cloud_init_and_friends(self):
-        self._add_package_mask('app-emulation/cloud-init', '>=app-emulation/cloud-init-0.7.7')
+        self._add_package_mask('app-emulation/cloud-init', '>=app-emulation/cloud-init-0.7.6_p1212')
         self._set_package_keywords('app-emulation/cloud-init', '**')  # TODO ~arch
         self._install_package_atoms(['app-emulation/cloud-init'])
         self.disable_cloud_init_syslog_fix_perms()
