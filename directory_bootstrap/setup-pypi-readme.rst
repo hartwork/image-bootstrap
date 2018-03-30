ffe94cc2e659245ee41386fd599c79fcc91674ab
Sebastian Pipping <sebastian@pipping.org>
2018-03-30 23:53:17 +0200

    Create meta-package "directory-bootstrap" for PyPI

---
+++ directory_bootstrap/setup-pypi-readme.rst
@@ -0,0 +1,15 @@
+``directory-bootstrap``
+=======================
+
+Command line tool for creating chroots.
+
+Technically a meta-package to pull in
+package `image-bootstrap <https://pypi.org/project/image-bootstrap/>`_,
+that contains the actual ``directory-bootstrap`` code,
+alongside its big brother ``image-bootstrap``
+that creates bootable virtual machine images.
+
+For more details on either of these please check out
+https://github.com/hartwork/image-bootstrap .
+
+Enjoy!
---
+++ directory_bootstrap/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python2
+# Copyright (C) 2018 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under AGPL v3 or later
+
+from textwrap import dedent
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+            name='directory-bootstrap',
+            description='Command line tool for creating chroots',
+            long_description=open('setup-pypi-readme.rst', 'r').read(),
+            license='AGPL v3 or later',
+            version='1',
+            author='Sebastian Pipping',
+            author_email='sebastian@pipping.org',
+            url='https://github.com/hartwork/image-bootstrap',
+            install_requires=[
+                'image-bootstrap',
+            ],
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'Intended Audience :: End Users/Desktop',
+                'Intended Audience :: System Administrators',
+                'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+                'Natural Language :: English',
+                'Operating System :: POSIX :: Linux',
+                'Programming Language :: Python :: 2.7',
+                'Topic :: System :: Installation/Setup',
+                'Topic :: Utilities',
+            ],
+    )
