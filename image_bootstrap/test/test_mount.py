f39db0fe44124ccde9bb5fa2ddf16261c2815dd1
Sebastian Pipping <sebastian@pipping.org>
2018-05-06 19:10:56 +0200

    Support "net:[..]" in /proc/*/mountinfo

--- image_bootstrap/test/test_mount.py
+++ image_bootstrap/test/test_mount.py
@@ -8,6 +8,7 @@ class TestMountInfoParser(TestCase):
     def test_line_parsing(self):
         for mount_info_line, expected_mount in (
                 ('17 21 0:4 / /proc rw,nosuid,nodev,noexec,relatime shared:12 - proc proc rw', '/proc'),
+                ('314 20 0:3 net:[4026532205] /run/docker/netns/8546120315b2 rw shared:124 - nsfs nsfs rw', '/run/docker/netns/8546120315b2'),
                 ):
             finder = MountFinder()
             groupdict = finder._parse_line(mount_info_line)
