728fc5bf6f5705ed54a5f0552e863ab18939b566
Sebastian Pipping <sebastian@pipping.org>
2020-05-21 15:22:58 +0200

    Bump version to 2.0.4

--- directory_bootstrap/shared/metadata.py
+++ directory_bootstrap/shared/metadata.py
@@ -7,8 +7,8 @@ GITHUB_HOME_URL = 'https://github.com/hartwork/image-bootstrap'
 
 DESCRIPTION = 'Command line tool for creating bootable virtual machine images'
 
-_VERSION = (2, 0, 3)
+_VERSION = (2, 0, 4)
 VERSION_STR = '.'.join((str(e) for e in _VERSION))
 
-_RELEASE_DATE = (2020, 4, 10)
+_RELEASE_DATE = (2020, 5, 21)
 RELEASE_DATE_STR = '-'.join(('%02d' % e for e in _RELEASE_DATE))
