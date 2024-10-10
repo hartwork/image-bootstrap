0f06aeac218beadfa91d3f4bc52ce84627d7934c
Sebastian Pipping <sebastian@pipping.org>
2024-10-11 00:49:36 +0200

    Drop Python 3.8 and add Python 3.13

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             author='Sebastian Pipping',
             author_email='sebastian@pipping.org',
             url=GITHUB_HOME_URL,
-            python_requires='>=3.8',
+            python_requires='>=3.9',
             setup_requires=[
                 'setuptools>=38.6.0',  # for long_description_content_type
             ],
@@ -74,7 +74,11 @@ if __name__ == '__main__':
                 'Operating System :: POSIX :: Linux',
                 'Programming Language :: Python',
                 'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
+                'Programming Language :: Python :: 3.11',
+                'Programming Language :: Python :: 3.12',
+                'Programming Language :: Python :: 3.13',
                 'Programming Language :: Python :: 3 :: Only',
                 'Topic :: System :: Installation/Setup',
                 'Topic :: Utilities',
